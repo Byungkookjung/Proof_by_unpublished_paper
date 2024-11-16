9335876b1fa7be42acc2beb7cad8076d95c1b21b
Byungkook Jung <152164635+Byungkookjung@users.noreply.github.com>
2024-11-16 14:38:45 -0700

    added js

---
+++ Website/js/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the necessary functions from the Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDcuafGs8GQeW0yR72Xd_EGmCF8mNUXxtM",
+    authDomain: "nathack2024.firebaseapp.com",
+    projectId: "nathack2024",
+    storageBucket: "nathack2024.firebasestorage.app",
+    messagingSenderId: "832945385242",
+    appId: "1:832945385242:web:97ce2f0c2625cf8b2186a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // Initialize Firestore
+
+export { db };
